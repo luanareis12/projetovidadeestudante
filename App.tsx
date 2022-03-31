a1fc03bd3f354cf99d88abda95adebd126823d09
Jairopedr <62018318+jairopedr@users.noreply.github.com>
2022-03-31 14:10:47 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import {ThemeProvider} from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 
 import { Home } from './src/screens/Home';
 import theme from './src/global/styles/theme';
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar style="auto" />
-      <Home />
+      <Home/>
     </ThemeProvider>
   );
 }
